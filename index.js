1e89807fb51533d76338a832d381392ea3708f6b
darshanbajania <darshanbajania1999@gmail.com>
2022-12-23 16:29:18 +0530

    change

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 var http = require('http');
 http.createServer(function (req, res) {
     console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
+    res.write(`Yo! ${req.url}`);
+
+
     res.end();
-    return "hello world"
+    // fetch('https://www.google.com/recaptcha/api/siteverify', {
+    //     method: 'POST',
+    //     // mode: 'no-cors',
+    //     headers: {
+    //         'Content-type': 'application/json; charset=UTF-8',
+    //     },
+    //     body: JSON.stringify({
+    //         secret: '6LfvcJ8jAAAAAGIDiRhcdKo5d3hGDmJ_s7RfhwVZ',
+    //         response: token
+    //     })
+    // }).then((res) => {
+    //     console.log(res)
+    // })
 }).listen(process.env.PORT || 3000);
